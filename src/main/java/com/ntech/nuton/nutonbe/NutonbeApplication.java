9cc22317c40a53342a6c9eec1f9ac84fdd28e720
Pranay Narotam <pnarotam@gmail.com>
2024-06-03 16:26:23 +1200

    init commit

---
+++ src/main/java/com/ntech/nuton/nutonbe/NutonbeApplication.java
@@ -0,0 +1,13 @@
+package com.ntech.nuton.nutonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutonbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutonbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntech/nuton/nutonbe/NutonbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntech.nuton.nutonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutonbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
